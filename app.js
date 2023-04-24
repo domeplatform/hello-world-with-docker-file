a90aa3b0112e54f1cf5f6ba92b9722a832cf7938
Jonathan Spruance <jspruance@inflightcorp.com>
2023-04-24 15:50:27 -0700

    Adding initial files

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    '<h2>POC - Build app from Docker image</h2><img src="https://cdn.cdnlogo.com/logos/d/17/docker.svg" width="400px"><p>This POC was brought to you by Dome Global engineering.</p>'
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
